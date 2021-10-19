7fc0df721cc7676a4d2e1368b3377db77a55a942
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-10-19 21:11:55 +0200

    snack 2 add form

--- snack-2/index.php
+++ snack-2/index.php
@@ -1,20 +1,60 @@
 <!-- Snack 2
 Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
-<?php  
+<?php
 
   $name = $_GET['name'];
   $email = $_GET['email'];
   $age = $_GET['age'];
 
+  $show = false;
   $access = false;
+  if ($name){
+    $show = true;
+  }
   if (strlen($name) > 3 && strpos($email, '@') != false && strpos($email, '.') != false && is_numeric($age)) {
     $access = true;
   };
 
-  if ($access) {
-    echo 'Accesso riuscito';
-  } else {
-    echo 'Accesso negato';
-  }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Snack 2</title>
+</head>
+
+<body>
+  <div>
+    <form action="">
+      <label for="name">Name: </label>
+      <input type="text" name="name" id="name">
+      <br>
+      <label for="email">email: </label>
+      <input type="text" name="email" id="email">
+      <br>
+      <label for="age">age: </label>
+      <input type="text" name="age" id="age">
+      <br>
+      <button type="submit">Access</button>
+    </form>
+  </div>
+  <div>
+    <?php 
+      if ($show) {
+        if ($access) {
+          echo '<h3>Accesso riuscito</h3>';
+        } else {
+          echo '<h3>Accesso negato</h3';
+        }
+      }
+    ?>  
+  <h3></h3>
+  </div>
+</body>
+
+</html>
