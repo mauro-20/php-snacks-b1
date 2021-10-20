bf37d183a9503704536f0750282f5c8172b5f6a0
Mauro Tomasoni <mauro.tom.vn@gmail.com>
2021-10-20 12:04:31 +0200

    snack 3

---
+++ snack-3/index.php
@@ -0,0 +1,78 @@
+<!-- Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. -->
+
+<?php
+
+$posts = [
+  '10/01/2019' => [
+    [
+      'title' => 'Post 1',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 1'
+    ],
+    [
+      'title' => 'Post 2',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 2'
+    ],
+  ],
+  '10/02/2019' => [
+    [
+      'title' => 'Post 3',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 3'
+    ]
+  ],
+  '15/05/2019' => [
+    [
+      'title' => 'Post 4',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 4'
+    ],
+    [
+      'title' => 'Post 5',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 5'
+    ],
+    [
+      'title' => 'Post 6',
+      'author' => 'Michele Papagni',
+      'text' => 'Testo post 6'
+    ]
+  ],
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Snack 3</title>
+</head>
+
+<body>
+  <ul>
+    <?php foreach ($posts as $date => $datePosts) : ?>
+    <li>
+        <h2><?= $date ?></h2>
+        <ul>
+          <?php foreach ($datePosts as $post) : ?>
+          <li>
+              <ul>
+                <?php foreach ($post as $value) : ?>
+                <li><?= $value ?></li>
+                <?php endforeach ?>
+              </ul>
+          </li>
+          <hr>
+          <?php endforeach ?>
+        </ul>
+    </li>
+    <?php endforeach ?>
+  </ul>
+</body>
+
+</html>
